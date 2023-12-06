370d9d66bce26719e4862373c2b19ac8d35a17c1
Fahad <145900785+Fahad00001@users.noreply.github.com>
2023-12-06 18:13:20 +0000

    datatypes conversion

---
+++ 01_basics/02_datatypesconversion.js
@@ -0,0 +1,17 @@
+let score="33abs"
+console.log(typeof score);
+console.log(typeof(score));
+ let valueinNumber=Number(score)
+ console.log(typeof valueinNumber)
+ console.log(valueinNumber)
+
+
+ let IsLoggedIn=true
+ let BooleanIsLoggedIn=Boolean(IsLoggedIn)
+ console.log(BooleanIsLoggedIn)
+
+
+ let somenumber=33
+ let stringnumber=String(somenumber)
+ console.log(stringnumber)
+ console.log(typeof stringnumber)
