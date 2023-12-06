3060cd6cbd2ae54661b4b4000ec7d4a24abad6a3
Fahad <145900785+Fahad00001@users.noreply.github.com>
2023-12-06 14:21:51 +0000

    container conf done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountid=12345
+let accountemail="fahad@gmail.com"
+var accountpaasword="rohan"
+accountcity="jaipur"
+
+//accountid=2
+// accountid=2345
+accountemail="hc@gmal.com"
+accountpaasword="mohan"
+accountcity="kolkata"
+console.table([accountid,accountemail,accountpaasword,accountcity])
+
+console.log(accountid);
