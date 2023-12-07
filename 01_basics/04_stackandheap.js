b63377058a45ffddbe86552e27b1fd2133eef888
Fahad <145900785+Fahad00001@users.noreply.github.com>
2023-12-07 08:10:36 +0000

    stack and heap

---
+++ 01_basics/04_stackandheap.js
@@ -0,0 +1,20 @@
+let myyoutubename="fahadwithcode"
+let anothername=myyoutubename
+anothername="fahadfitness"
+console.log(anothername);//fahadfitnesss
+console.log(myyoutubename);//fahadwithcode
+
+let userone={
+    email:"user@google.com",
+    upi: "user@abi"
+
+
+}
+
+ let usertwo=userone
+ usertwo.email="fahad@gamil.com"
+ usertwo.upi="fhaad"
+ console.log(userone.email);//fahad@gamil.com
+ console.log(usertwo.email);//fahad@gamil.com
+console.log(userone.upi);
+console.log(usertwo.upi);
