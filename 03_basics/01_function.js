a16c15e7dddd789700c0a7dfb968c0890e7c9d69
Fahad <145900785+Fahad00001@users.noreply.github.com>
2023-12-09 15:32:01 +0000

    01_function updated

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -15,3 +15,20 @@ console.log("please enter your name");
     return `${username} just logged in`
 }
 console.log(loginUserMessage());
+
+
+const user={
+    username:"fahad",
+    price:399
+}
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+handleObject(user)
+
+const mynewarray=[100,2000,300]
+function returnSecondValue(getarray){
+    return getarray[1]
+}
+console.log(returnSecondValue(mynewarray));
+console.log(returnSecondValue[100,2000,300]);
