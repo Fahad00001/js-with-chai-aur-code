03c4a2ff1a20c75df4ddeaa02fa10fe001096d99
Fahad <145900785+Fahad00001@users.noreply.github.com>
2023-12-20 09:27:54 +0530

    Update chaiaurcode.js

--- clock project/chaiaurcode.js
+++ clock project/chaiaurcode.js
@@ -4,5 +4,6 @@ const clock=document.getElementById('clock')
 // console.log(date.toLocaleString());
 setInterval(function(){
     let date=new Date();
-    console.log(date.toLocaleString()); 
+    // console.log(date.toLocaleString()); 
+    clock.innerHTML=date.toLocaleString();
 },1000)
